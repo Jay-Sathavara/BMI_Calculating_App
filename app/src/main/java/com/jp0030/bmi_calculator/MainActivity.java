0c945d5c0fcc2ad66edf6dc7e84183e75fc4a6be
JP0030 <thekeen003@gmail.com>
2024-02-04 18:07:30 +0530

    full app is ready with unattractive UI.

--- app/src/main/java/com/jp0030/bmi_calculator/MainActivity.java
+++ app/src/main/java/com/jp0030/bmi_calculator/MainActivity.java
@@ -1,8 +1,13 @@
 package com.jp0030.bmi_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText edtWeight , edtHigthFt , edtHightIn ;
+        AppCompatButton btnCalculate;
+        TextView txtResult;
+        LinearLayout llMain ;
+
+        edtWeight = findViewById(R.id.edtWeight);
+        edtHigthFt = findViewById(R.id.edtHightFt);
+        edtHightIn = findViewById(R.id.edtHightIn);
+        btnCalculate = findViewById(R.id.btnCalculate);
+        txtResult = findViewById(R.id.txtResult);
+        llMain = findViewById(R.id.llMain);
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int wt = Integer.parseInt(edtWeight.getText().toString());
+                int ft = Integer.parseInt(edtHigthFt.getText().toString());
+                int in = Integer.parseInt(edtHightIn.getText().toString());
+
+                int totalIn = ft*12 + in ;
+                double totaleCm = totalIn*2.53 ;
+                double totaleM = totaleCm/100;
+
+                double bmi = wt/(totaleM*totaleM);
+
+                if (bmi>25){
+                    txtResult.setText("You're over Waight");
+                    llMain.setBackgroundColor(getResources().getColor(R.color.OW));
+                }else if (bmi<18){
+                    txtResult.setText("You're UndrWaight");
+                    llMain.setBackgroundColor(getResources().getColor(R.color.UW));
+                }else {
+                    txtResult.setText("You're Healthy");
+                    llMain.setBackgroundColor(getResources().getColor(R.color.HE));
+                }
+            }
+        });
+
     }
 }
